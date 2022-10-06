e384406a9ec09dcc1b2f12f10e05c5053ad0bab0
Samuel Mayers <36217260+samuelmayers@users.noreply.github.com>
2022-10-06 16:00:57 -0400

    Update and rename init.sql to my.cnf

--- init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "form"
-(
-    "id" INTEGER NOT NULL,
-    "fullname" VARCHAR(200) NOT NULL,
-    "phone" VARCHAR(200) NOT NULL,
-    "email" VARCHAR(200) NOT NULL UNIQUE,
-    PRIMARY KEY("id")
-);
